78289bb7ddbf8778c35f1838aee3de2b4d660e9d
Alejandra Sanchez <alejandra.sanchez@babelgroup.com>
2023-09-29 10:10:53 +0200

    boton-fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import Switch from './components/Switch';
 import Footer from './components/Footer/footer';
+import {useState} from 'react'
 
 const App = () => {
+  const [isBackgroundPink, setIsBackgroundPink] = useState('true');/* pone predeterminado el fondo rosa= true*/
+
+  const toggleBackgroundColor= () => { /* cambia el estado*/
+    setIsBackgroundPink(!isBackgroundPink); /* cambia al valor contrario*/
+
+  }
+
+  const appStyle = {
+    backgroundColor: isBackgroundPink ? 'pink' : 'white', 
+    minHeight: '100vh', 
+    transition: 'background-color 0.3s ease-in-out',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
   return (
-    <>
-      <Switch />
-      <div>
+   
+    <div style = {appStyle}>
+        <Switch />
+        <button onClick = {toggleBackgroundColor}> 
+        {isBackgroundPink ? 'Restaurar color' : 'Cambiar color'}
+        </button>
+        <div>
       <Footer />
       </div>
-    </>
-  );
+    </div>
+ 
+  )
 };
+
+
+
+
 
 export default App;
